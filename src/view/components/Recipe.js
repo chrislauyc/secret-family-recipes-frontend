997b2045925e18ad0fb26acad3ba7735e9b48a8f
unknown <jlarson129@outlook.com>
2021-09-22 19:48:54 -0400

    started some of the recipe page

--- src/view/components/Recipe.js
+++ src/view/components/Recipe.js
@@ -1,23 +1,30 @@
-import React, { useContext } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
-// import { makeStyles } from "@material-ui/core/styles";
-import { Button, Grid, Typography, Paper, TextField } from "@material-ui/core";
-import { RecipeContext } from "../../context/RecipeContext";
+// , { useContext } 
+import { Button, Grid, Typography, Paper } from "@material-ui/core";
+// import { RecipeContext } from "../../context/RecipeContext";
+
 
 // import { axiosWithAuth } from "../../helpers/axiosWithAuth";
 // import axios from "axios";
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     "& .MuiTextField-root": {
-//       margin: theme.spacing(1),
-//       width: "25ch",
-//     },
-//   },
-// }));
+
+
+ const recipe = {
+   "recipe_id":1,
+   "user_id":1,
+   "source":"grandmother",
+   "category":"dinner",
+   "recipe_name":"tacos",
+   "image_url":"https://www.thewholesomedish.com/wp-content/uploads/2019/06/The-Best-Classic-Tacos-550.jpg",
+   "steps":{"description":"cook them",
+   "ingredients":[
+     {"ingredient_name":"taco shell","amount":10,"unit":"none"},
+     {"ingredient_name":"miced beef","amount":125,"unit":"gram"}]}}
 
 export default function Recipe() {
-  const { recipe } = useContext(RecipeContext);
+  // const { recipe } = useContext(RecipeContext);
+  // const recipe = cardsInformation[1]
   const history = useHistory();
 //   const classes = useStyles();
   // const { id } = useParams();
@@ -35,39 +42,22 @@ export default function Recipe() {
 
   return (
    <Paper>
-       <Grid container alignItems="center" justifyContent="center" style={{ minHeight: "100vh" }}>
-         <Typography variant="h5" component="h2">
-           Edit Recipe {recipe.name}
-         </Typography>{" "}
-         <br />
-         <Grid item xs={12}>
-           <div>
-             <TextField id="outlined-basic" name="name" label="Name" variant="outlined" value={recipe.name} />
-           </div>
-           <div>
-             {" "}
-             <TextField id="outlined-basic" name="source" label="Source" variant="outlined" value={recipe.source} />
-           </div>
-           <div>
-             <TextField id="outlined-multiline-static" name="description" label="Description" multiline rows={8} variant="outlined" value={recipe.description} />
-           </div>
-           <div>
-             <TextField id="outlined-multiline-static" name="ingredients" label="Ingredients" multiline rows={8} variant="outlined" value={recipe.Ingredients} />
-           </div>
-           <div>
-             <TextField id="outlined-multiline-static" name="instructions" label="Instructions" multiline rows={8} variant="outlined" value={recipe.instructions} />
-           </div>
-           <div>
-             <Grid container>
-               <Grid item>
-                 <Button onClick={handleReturn} variant="contained" size="small">
-                   Go Back
-                 </Button>
-               </Grid>
-             </Grid>
-           </div>
+       <Grid container direction="column" justifyContent="center" style={{ height: "100vh", width:"80vh" }}>
+         <Grid item xs={2}>
+          <Grid container>
+            <Grid item>
+              <Typography variant="h4" component="h2">
+              {recipe.recipe_name}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Button onClick={handleReturn} variant="contained" size="small">
+                Home
+              </Button>
+            </Grid>
          </Grid>
        </Grid>
+      </Grid>
    </Paper>
   );
 }
