b6f742ea14171d395670b65ce9f0405d1dff86b1
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-22 09:13:44 -0700

    hitting object error

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -3,13 +3,13 @@ import axios from "axios";
 //Task List:
 //Build and export a function used to send in our authorization token
 export const axiosWithAuth = () => {
-  return axios.create({
-    baseURL: "https://family-recipes-app.herokuapp.com/mock/recipes"
-  })
   // return axios.create({
-  //   baseURL: `https://family-recipes-app.herokuapp.com/api/${localStorage.getItem("id")}`,
-  //   headers: {
-  //     Authorization: localStorage.getItem("token"),
-  //   },
-  // });
+  //   baseURL: "https://family-recipes-app.herokuapp.com/mock/recipes"
+  // })
+  return axios.create({
+    baseURL: `https://family-recipes-app.herokuapp.com/api/${localStorage.getItem("id")}`,
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+  });
 };
