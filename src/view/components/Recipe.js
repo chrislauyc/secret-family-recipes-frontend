a259e8cd44dcc4a59177208c9c9fd359b3e42a43
unknown <jlarson129@outlook.com>
2021-09-25 21:24:28 -0400

    added header, img/ingredient section, and built ingredients and steps api rendering

--- src/view/components/Recipe.js
+++ src/view/components/Recipe.js
@@ -1,24 +1,84 @@
 import React, { useEffect, useState }  from "react";
 import { useHistory } from "react-router-dom";
 import { useParams } from "react-router";
-import { Button, Grid, Typography } from "@material-ui/core";
-
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import HomeIcon from '@mui/icons-material/Home';
 import { axiosWithAuth } from "../../helpers/axiosWithAuth";
+import { Checkbox } from "@mui/material";
 
 
+
+
+const useStyles = makeStyles((theme) => ({
+    titleContainer: {
+      display: "flex",
+      justifyContent: "space-around",
+      width: "",
+      alignItems:"center",
+      borderBottom: "5px solid #FF6969"
+    },
+    recipeContainer: {
+        maxWidth:"80rem",
+        margin:"auto"
+    },
+    introContainer: {
+        height: "35rem",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+        
+    },
+    image: {
+        width: "60%",
+        height: "100%",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        backgroundSize: "contain"
+    },
+    ingredients: {
+        width: "35%",
+        height: "fit-content",
+        border: "3px dashed grey",
+        borderRadius: "10px",
+        backgroundColor: "#FC977B",
+        
+    },
+    ingTitle: {
+        width: "100%",
+        textAlign: "center"
+    },
+    ingredient: {
+        "& p": {
+            display:"inline"
+        },
+    },
+    name: {
+        marginLeft: ".5rem"
+    },
+    amount: {
+        marginRight: ".2rem"
+    },
+    steps: {
+        
+    }
+  }));
 
 
 export default function Recipe() {
   const [recipe, setRecipe] = useState({});
+  const [name, setName] = useState("")
   const history = useHistory();
-  //   const classes = useStyles();
+  const classes = useStyles();
   const { id } = useParams();
+
   useEffect(() => {
     axiosWithAuth()
       .get(`/recipes/${id}`)
       .then((res) => {
         console.log("respone: ", res.data);
         setRecipe(res.data);
+        setName(res.data.recipe_name.toUpperCase())
       })
       .catch((err) => {
         console.log({err});
@@ -32,22 +92,54 @@ export default function Recipe() {
     history.push("/home");
   };
 
+
   return (
-       <Grid container direction="column" alignContent="center" justifyContent="center" >
-         <Grid item lg={12}>
-          <Grid container justifyContent="space-between" style={{width:"80vh"}}>
-            <Grid item xs={12} md={2} lg={2}>
-              <Typography variant="h4" component="h2">
-              {recipe.recipe_name}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Button onClick={handleReturn} variant="contained" size="small">
-                Home
-              </Button>
-            </Grid>
-         </Grid>
-       </Grid>
-      </Grid>
+       <div className={classes.recipeContainer}>
+           <div className={classes.titleContainer}>
+               <h1>{name}</h1>
+
+               <Button variant="contained" startIcon={<HomeIcon/>} onClick={handleReturn} style={{height:"fit-content"}}>
+                   Home
+               </Button>
+           </div>
+           <div className={classes.introContainer}>
+               <div className={classes.image} style={{backgroundImage: `url(${recipe.image_url})`}}/>
+               <div className={classes.ingredients}>
+                   <div className={classes.ingTitle}><h3>Ingredients</h3></div>
+                   <div className={classes.ingredient}>
+                        <Checkbox/>
+                        <p className={classes.amount}>amount</p>
+                        <p className={classes.unit}>unit</p>
+                        <p className={classes.name}>name</p>
+                    </div>
+                    <div className={classes.ingredient}>
+                        <Checkbox/>
+                        <p className={classes.amount}>amount</p>
+                        <p className={classes.unit}>unit</p>
+                        <p className={classes.name}>name</p>
+                    </div>
+                   {/* {recipe.ingredients.map(ingredient => {
+                       return(
+                           <div className={classes.ingredient}>
+                               <Checkbox/>
+                               <p className={classes.amount}>{ingredient.amount}</p>
+                               <p className={classes.unit}>{ingredient.unit}</p>
+                               <p className={classes.name}>{ingredient.name}</p>
+                           </div>
+                       )
+                   })} */}
+               </div>
+           </div>
+           <div className={classes.steps}>
+               {/* {recipe.steps.map((step,int) => {
+                   return(
+                       <div className={classes.step}>
+                           <p>step {int}: <br/> {step}</p>
+                       </div>
+                   )
+               })} */}
+           </div>
+       </div>
   );
 }
+// recipe.image_url
